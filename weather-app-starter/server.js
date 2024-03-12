389feae5f1bd9739eb2c995e050aaafc4a155a40
emaniprateek <prateek3979@gmail.com>
2024-03-12 17:03:36 +0530

    node js weather app

--- weather-app-starter/server.js
+++ weather-app-starter/server.js
@@ -17,9 +17,12 @@ app.get("/", (req, res) => {
 app.get("/weather", async (req, res) => {
   // Get the city from the query parameters
   const city = req.query.city;
-  const apiKey = "";
+  const apiKey = "4ab76c4d7fb9693705fedb6a7f8183a6";
 
   // Add your logic here to fetch weather data from the API
+
+
+
   const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
   let weather;
   let error = null;
